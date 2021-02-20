769cbe93098fcf2a2d52cb8518b35081f6226422
hbjang <13klove@naver.com>
2021-02-20 17:44:09 +0900

    어디로 가는지 확인

--- api/src/main/java/com/skel/pro/api/ApiApplication.java
+++ api/src/main/java/com/skel/pro/api/ApiApplication.java
@@ -6,7 +6,7 @@ import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class ApiApplication {
-
+//주석으로 확인
     public static void main(String[] args) { run(ApiApplication.class, args); }
 
 }
