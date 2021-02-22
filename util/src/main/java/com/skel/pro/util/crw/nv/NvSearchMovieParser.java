c3cc022c127263d3552beb25ec16cc3512c6275b
hbjang <13klove@naver.com>
2021-02-22 23:09:39 +0900

    너는 너가 아닌 하나님이 이끄는 길로 갈거야

--- util/src/main/java/com/skel/pro/util/crw/nv/NvSearchMovieParser.java
+++ util/src/main/java/com/skel/pro/util/crw/nv/NvSearchMovieParser.java
@@ -14,20 +14,20 @@ import java.io.IOException;
 public class NvSearchMovieParser implements CrwParser {
 
     private final CloseableHttpClient client;
-    private final String host = "https://movie.naver.com/movie/running/current.nhn";
+    private final String host = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%98%81%ED%99%94";
 
     @Override
     public void read(CrwVo vo) throws IOException {
         HttpGet httpGet = new HttpGet(host);
-        httpGet.setHeader(":authority", "movie.naver.com");
+        httpGet.setHeader(":authority", "search.naver.com");
         httpGet.setHeader(":method", "GET");
-        httpGet.setHeader(":path", "/movie/running/current.nhn");
+        httpGet.setHeader(":path", "/search.naver?where=nexearch&sm=tab_etc&query=%ED%98%84%EC%9E%AC%EC%83%81%EC%98%81%EC%98%81%ED%99%94");
         httpGet.setHeader(":scheme", "https");
         httpGet.setHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
         httpGet.setHeader("accept-encoding", "gzip, deflate, br");
         httpGet.setHeader("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
-        httpGet.setHeader("cookie", "NNB=SVKQOJKDFUVGA; nx_ssl=2; NRTK=ag#20s_gr#0_ma#-2_si#-2_en#-2_sp#-2; NDARK=Y; nid_inf=715548050; NID_AUT=SJTfXiDyHPDG197leY2SMIKITNcSMUZ9avlbJ0wOigT/AMzY2sHauqdJXqeE3W0p; NID_JKL=ejAPJF7aKmdDDH/lnrl1u0h0BvhzO+eTYJY6U25glRg=; BMR=s=1613959934799&r=https%3A%2F%2Fm.blog.naver.com%2FPostView.nhn%3FblogId%3Dkop_seop%26logNo%3D220730943499%26proxyReferer%3Dhttps%3A%252F%252Fwww.google.com%252F&r2=https%3A%2F%2Fwww.google.com%2F; page_uid=hvLNFsp0JXossCO/vtlssssst4o-164570; JSESSIONID=C1EC7D928C04C3B5FD00BB0DBA5BA83E; NM_THUMB_PROMOTION_BLOCK=Y; NID_SES=AAABm1jHV8kwklDeFSjVBb+W0g61IgaA1BE0wUit2J0fZP34hG90SMXWlp1oR8tC2CVNxeMze6OMILmyNNEqC2T32ALWl2UW1MToVbIfSSOJDTSYCkbulf4/GzZBnbksUg2BO7cQ7qVb+w4pmMnffqaglQurH2C5uxpmITED+0JV5ejSq62AqPaHMCvB2I+n+38ymG/EYUC9Cq9lX4HFmotXnsxli04uJJmJm5oZIGpUTnzGCt8JXdVVHw8a6jfSevVhWCe65+T2NavmZhrFIyGkFE76Om2e5O2zjS/6Bt+bT+Ov3V8QHQqoBlfnFpED7cFGNxJU2qvii6ls4sFIk61e7C3mbQyKvJEqfMsGXwIUkkhhWLmxklStLno+oQ0hu6ylSPXKNvU3S8B/vRpAcTsyxVGyiP3WdyZLjbwQdaKvweT4nVIZpDieAV5UTdUZcojfaMDxD9shNjzZf8jjnhC1LXPCPN4PtK+Scb+z6GuKr9h5A+g2Njxd/MGDFZZBcuESv0KCkUUQT07mO4rlDx1LynON8fB7JGfVcqMAobwjXyvX; csrf_token=35d4a4e9-7324-429f-8dd3-726ad6b56d3a");
-        httpGet.setHeader("referer", "https://movie.naver.com/movie/running/current.nhn");
+        httpGet.setHeader("cache-control", "max-age=0");
+        httpGet.setHeader("cookie", "NNB=JFN3MB3FE4PV4; ASID=dc464634000001708ea01af700000058; _ga_7VKFYR6RV1=GS1.1.1598192351.1.0.1598192355.56; _fbp=fb.1.1598779522147.1505056206; _ga_4BKHBFKFK0=GS1.1.1600255475.1.1.1600255531.4; NM_THUMB_PROMOTION_BLOCK=Y; MM_NEW=1; NFS=2; MM_NOW_COACH=1; m_loc=dc3a87521af7ac8085550656fd167f30bbc367e4c7b103f301b5d3687f3c4e4fca74d1f38b5d46de2c1d2a842f0e4f671207e3757267f07d65eaea10192440b991184833f9cac50782ca4628737fbcfb; NRTK=ag#20s_gr#0_ma#-2_si#-2_en#-2_sp#-2; _ga_1BVHGNLQKG=GS1.1.1608807017.1.1.1608807035.0; _ga=GA1.2.1592814637.1592707957; NV_WETR_LAST_ACCESS_RGN_M=\"MDk2MjA2MDU=\"; NV_WETR_LOCATION_RGN_M=\"MDk2MjA2MDU=\"; nx_ssl=2; _gid=GA1.2.1937057137.1613915712");
         httpGet.setHeader("sec-fetch-dest", "document");
         httpGet.setHeader("sec-fetch-mode", "navigate");
         httpGet.setHeader("sec-fetch-site", "same-site");
